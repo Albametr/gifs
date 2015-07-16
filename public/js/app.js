e48ce7d8e172ed9add17056672e349289dcf36bd
altuhov <altuhov@clickberry.com>
2015-07-16 19:10:34 +0300

    support ie & poc different render methods

--- public/js/app.js
+++ public/js/app.js
@@ -34,8 +34,8 @@ angular.module('directives', [])
                 var video = $video[0];
 
                 $video.on('play', function () {
-                    var interval;
-                    interval = setInterval(function () {
+                    var count = 0;
+                    var interval = function () {
                         var canvas, ctx;
                         if (video.currentTime <= end && video.currentTime >= start) {
                             canvas = document.createElement('canvas');
@@ -43,26 +43,67 @@ angular.module('directives', [])
                             canvas.height = height;
                             ctx = canvas.getContext('2d');
                             ctx.drawImage(video, 0, 0);
-                            images.push(canvas);
+                            var frame = ctx.getImageData(0, 0, width, height);
+                            //var frame=canvas.toDataURL("image/jpeg", 1.0);
+
+                            images.push(frame);
                             if (images.length === 1) {
                                 drawFrame(0, images);
                             }
                         } else if (video.currentTime > end) {
-                            clearInterval(interval);
                             video.pause();
                         }
-                    }, 40);
+
+                        setTimeout(interval, 40);
+                    };
+
+                    interval();
+                    //var interval;
+                    //interval = setInterval(function () {
+                    //    var canvas, ctx;
+                    //    if (video.currentTime <= end && video.currentTime >= start) {
+                    //        canvas = document.createElement('canvas');
+                    //        canvas.width = width;
+                    //        canvas.height = height;
+                    //        ctx = canvas.getContext('2d');
+                    //        ctx.drawImage(video, 0, 0);
+                    //        images.push(canvas);
+                    //        if (images.length === 1) {
+                    //            drawFrame(0, images);
+                    //        }
+                    //    } else if (video.currentTime > end) {
+                    //        clearInterval(interval);
+                    //        video.pause();
+                    //    }
+                    //}, 40);
                 });
 
                 var $targetCanvas = $element.find('canvas.gif-container');
                 var targetCanvasCtx = $targetCanvas[0].getContext('2d');
+                var targetImg = $element.find('img.img-container')[0];
                 var drawFrame = function (index, images) {
                     var data;
                     if (index < 0 || index >= images.length) {
                         return;
                     }
                     data = images[index];
-                    targetCanvasCtx.drawImage(data, 0, 0);
+                    //targetCanvasCtx.drawImage(data, 0, 0);
+                    targetCanvasCtx.putImageData(data, 0, 0);
+                    //targetImg.src=data;
+                    //var image = new Image();
+                    //image.src = data;
+                    //image.onload = function(){
+                    //    targetCanvasCtx.drawImage(image, 0,0);
+                    //};
+                };
+
+                var prevIndex=0;
+                var drawFrame_2=function(index){
+                    if(Math.abs(index-prevIndex)>=0.09) {
+                        video.currentTime = index;
+                        targetCanvasCtx.drawImage(video, 0, 0);
+                        prevIndex=index;
+                    }
                 };
 
                 var position = {x: 0, prevX: 0};
@@ -71,7 +112,7 @@ angular.module('directives', [])
                         e = e.originalEvent;
                     }
 
-                    position.x =getX(e);
+                    position.x = getX(e);
 
                     $targetCanvas.bind('mousemove', trackMove);
                     $targetCanvas.bind('touchmove', trackMove);
@@ -106,6 +147,8 @@ angular.module('directives', [])
                         idx = parseInt(x / width * images.length);
                         idx = Math.min(idx, images.length - 1);
                         drawFrame(idx, images);
+                        //var index=x / width * (end-start);
+                        //drawFrame_2(index);
                     }
                     e.preventDefault();
                 };
@@ -113,19 +156,6 @@ angular.module('directives', [])
                 function getX(e) {
                     //return (e.touches && e.touches[0].clientX) || e.offsetX;
                     return (e.changedTouches && e.changedTouches[0].clientX) || e.offsetX;
-                };
-
-                var trackTouchMove = function (e) {
-                    var idx, x, y;
-                    if (e.originalEvent) {
-                        e = e.originalEvent;
-                    }
-                    x = e.touches[0].clientX;
-                    y = e.touches[0].clientY;
-                    idx = parseInt(x / width * images.length);
-                    idx = Math.min(idx, images.length - 1);
-                    drawFrame(idx, images);
-                    e.preventDefault();
                 };
 
                 $targetCanvas.bind('mousedown', mouseDown);
